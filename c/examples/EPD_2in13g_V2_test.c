e3ac1e9b515a43d97d8f8fd4a661d27d7a8aa71a
Vincent Udén <vincentuden@gmail.com>
2026-04-09 17:17:38 +0200

    Shorten example

--- c/examples/EPD_2in13g_V2_test.c
+++ c/examples/EPD_2in13g_V2_test.c
@@ -33,132 +33,31 @@
 
 int EPD_2in13g_V2_test(void)
 {
-    printf("EPD_2IN13G_V2_test Demo\r\n");
+    printf("EPD_2IN13G_V2_test Demo\n");
     if(DEV_Module_Init()!=0){
         return -1;
     }
 
-    printf("e-Paper Init and Clear...\r\n");
-    // TODO: We get stuck here if the program has been ran before. Maybe because the screen is in sleep mode
+    printf("e-Paper Init...\n");
     EPD_2IN13G_V2_Init();
 
-    struct timespec start={0,0}, finish={0,0}; 
-    clock_gettime(CLOCK_REALTIME, &start);
+    printf("e-Paper Clear white...\n");
     EPD_2IN13G_V2_Clear(EPD_2IN13G_V2_WHITE); // White
-    clock_gettime(CLOCK_REALTIME, &finish);
-    printf("%ld S\r\n", finish.tv_sec-start.tv_sec);	
+
     DEV_Delay_ms(2000);
 
-    //Create a new image cache
-    UBYTE *BlackImage;
-    UWORD Imagesize = ((EPD_2IN13G_V2_WIDTH % 4 == 0)? (EPD_2IN13G_V2_WIDTH / 4 ): (EPD_2IN13G_V2_WIDTH / 4 + 1)) * EPD_2IN13G_V2_HEIGHT;
-    if((BlackImage = (UBYTE *)malloc(Imagesize)) == NULL) {
-        printf("Failed to apply for black memory...\r\n");
-        return -1;
-    }
-    printf("Paint_NewImage\r\n");
-    Paint_NewImage(BlackImage, EPD_2IN13G_V2_WIDTH, EPD_2IN13G_V2_HEIGHT, 0, EPD_2IN13G_V2_WHITE);
-    Paint_SetScale(4);
+    printf("e-Paper Clear black...\n");
+    EPD_2IN13G_V2_Clear(EPD_2IN13G_V2_BLACK); // White
 
-#if 1   // show bmp
-    printf("show BMP-----------------\r\n");
-    Paint_SelectImage(BlackImage);
-    GUI_ReadBmp_RGB_4Color("./pic/2in13g.bmp", 0, 0);
-    EPD_2IN13G_V2_Display(BlackImage);
     DEV_Delay_ms(2000);
-#endif
 
-#if 1   // Drawing on the image
-    EPD_2IN13G_V2_Init_Fast();
-    Paint_NewImage(BlackImage, EPD_2IN13G_V2_WIDTH, EPD_2IN13G_V2_HEIGHT, 90, EPD_2IN13G_V2_WHITE);
-    Paint_SetScale(4);
-    //1.Select Image
-    printf("SelectImage:BlackImage\r\n");
-    Paint_SelectImage(BlackImage);
-    Paint_Clear(EPD_2IN13G_V2_WHITE);
-
-    // 2.Drawing on the image
-    printf("Drawing:BlackImage\r\n");
-    Paint_DrawPoint(10, 80, EPD_2IN13G_V2_RED, DOT_PIXEL_1X1, DOT_STYLE_DFT);
-    Paint_DrawPoint(10, 90, EPD_2IN13G_V2_YELLOW, DOT_PIXEL_2X2, DOT_STYLE_DFT);
-    Paint_DrawPoint(10, 100, EPD_2IN13G_V2_BLACK, DOT_PIXEL_3X3, DOT_STYLE_DFT);
-    Paint_DrawLine(20, 70, 70, 120, EPD_2IN13G_V2_RED, DOT_PIXEL_1X1, LINE_STYLE_SOLID);
-    Paint_DrawLine(70, 70, 20, 120, EPD_2IN13G_V2_RED, DOT_PIXEL_1X1, LINE_STYLE_SOLID);
-    Paint_DrawRectangle(20, 70, 70, 120, EPD_2IN13G_V2_YELLOW, DOT_PIXEL_1X1, DRAW_FILL_EMPTY);
-    Paint_DrawRectangle(80, 70, 130, 120, EPD_2IN13G_V2_YELLOW, DOT_PIXEL_1X1, DRAW_FILL_FULL);
-    Paint_DrawCircle(45, 95, 20, EPD_2IN13G_V2_BLACK, DOT_PIXEL_1X1, DRAW_FILL_EMPTY);
-    Paint_DrawCircle(105, 95, 20, EPD_2IN13G_V2_BLACK, DOT_PIXEL_1X1, DRAW_FILL_FULL);
-    Paint_DrawLine(85, 95, 125, 95, EPD_2IN13G_V2_RED, DOT_PIXEL_1X1, LINE_STYLE_DOTTED);
-    Paint_DrawLine(105, 75, 105, 115, EPD_2IN13G_V2_YELLOW, DOT_PIXEL_1X1, LINE_STYLE_DOTTED);
-    Paint_DrawString_EN(0, 0, "Red,yellow,white,black", &Font16, EPD_2IN13G_V2_RED, EPD_2IN13G_V2_YELLOW);
-    Paint_DrawString_EN(0, 16, "Four color e-Paper", &Font12, EPD_2IN13G_V2_YELLOW, EPD_2IN13G_V2_BLACK);
-    Paint_DrawString_CN(120, 28, "Î¢Ñ©µç×Ó", &Font24CN, EPD_2IN13G_V2_RED, EPD_2IN13G_V2_WHITE);
-    Paint_DrawNum(0, 28, 123456, &Font12, EPD_2IN13G_V2_RED, EPD_2IN13G_V2_WHITE);
-
-    printf("EPD_Display\r\n");
-    EPD_2IN13G_V2_Display(BlackImage);
-    DEV_Delay_ms(3000);
-#endif
-
-    Paint_NewImage(BlackImage, EPD_2IN13G_V2_WIDTH, EPD_2IN13G_V2_HEIGHT, 0, EPD_2IN13G_V2_WHITE);
-    Paint_SetScale(4);
-
-#if 1   // Drawing on the image
-    //1.Select Image
-    printf("SelectImage:BlackImage\r\n");
-    Paint_SelectImage(BlackImage);
-    Paint_Clear(EPD_2IN13G_V2_WHITE);
-
-    // 2.Drawing on the image
-    printf("Drawing:BlackImage\r\n");
-    Paint_DrawRectangle(1, 1, EPD_2IN13G_V2_WIDTH, EPD_2IN13G_V2_HEIGHT/4, EPD_2IN13G_V2_RED, DOT_PIXEL_1X1, DRAW_FILL_FULL);
-    Paint_DrawRectangle(1, EPD_2IN13G_V2_HEIGHT/4, EPD_2IN13G_V2_WIDTH, EPD_2IN13G_V2_HEIGHT/2, EPD_2IN13G_V2_YELLOW, DOT_PIXEL_1X1, DRAW_FILL_FULL);
-    Paint_DrawRectangle(1, EPD_2IN13G_V2_HEIGHT/2, EPD_2IN13G_V2_WIDTH, 3*EPD_2IN13G_V2_HEIGHT/4, EPD_2IN13G_V2_BLACK, DOT_PIXEL_1X1, DRAW_FILL_FULL);
-
-    printf("EPD_Display\r\n");
-    EPD_2IN13G_V2_Display(BlackImage);
-    DEV_Delay_ms(3000);
-#endif
-
-#if 1   // Drawing on the image
-    //1.Select Image
-    printf("SelectImage:BlackImage\r\n");
-    Paint_SelectImage(BlackImage);
-    Paint_Clear(EPD_2IN13G_V2_WHITE);
-
-    int hNumber, hWidth, vNumber, vWidth;
-    hNumber = 20;
-	hWidth = EPD_2IN13G_V2_HEIGHT/hNumber;
-    vNumber = 8;
-	vWidth = EPD_2IN13G_V2_WIDTH/vNumber;
-
-    // 2.Drawing on the image
-    printf("Drawing:BlackImage\r\n");
-	for(int i=0; i<hNumber; i++) {  // horizontal
-		Paint_DrawRectangle(1, 1+i*hWidth, EPD_2IN13G_V2_WIDTH, hWidth*(1+i), EPD_2IN13G_V2_BLACK + (i % 2), DOT_PIXEL_1X1, DRAW_FILL_FULL);
-	}
-	for(int i=0; i<vNumber; i++) {  // vertical
-        if(i%2) {
-            Paint_DrawRectangle(1+i*vWidth, 1, vWidth*(i+1), EPD_2IN13G_V2_HEIGHT, EPD_2IN13G_V2_YELLOW + (i/2%2), DOT_PIXEL_1X1, DRAW_FILL_FULL);
-        }
-	}
-    printf("EPD_Display\r\n");
-    EPD_2IN13G_V2_Display(BlackImage);
-    DEV_Delay_ms(3000);
-#endif
-
-    printf("Clear...\r\n");
-    EPD_2IN13G_V2_Init();
+    printf("Clear...\n");
     EPD_2IN13G_V2_Clear(EPD_2IN13G_V2_WHITE);
 
-    printf("Goto Sleep...\r\n");
-    EPD_2IN13G_V2_Sleep();
-    free(BlackImage);
-    BlackImage = NULL;
     DEV_Delay_ms(2000);//important, at least 2s
-    // close 5V
     printf("close 5V, Module enters 0 power consumption ...\r\n");
-    DEV_Module_Exit();
+
+    // DEV_Module_Exit();
     
     return 0;
 }
